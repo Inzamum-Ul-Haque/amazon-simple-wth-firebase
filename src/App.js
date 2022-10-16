289110f3f15afcc6b5b2fd2481cfee7e9f9e35cc
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-17 02:06:44 +0600

    login page with stylings added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Shop from "./components/Shop/Shop";
 import Orders from "./components/Orders/Orders";
 import Inventory from "./components/Inventory/Inventory";
 import { productsAndCartLoader } from "./loaders/productsAndCartLoader";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   const router = createBrowserRouter([
@@ -19,16 +21,24 @@ function App() {
           element: <Shop />,
         },
         {
-          path: "orders",
+          path: "/orders",
           loader: productsAndCartLoader,
           element: <Orders />,
         },
         {
-          path: "inventory",
+          path: "/inventory",
           element: <Inventory />,
         },
         {
-          path: "about",
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/signup",
+          element: <SignUp />,
+        },
+        {
+          path: "/about",
           element: <About />,
         },
       ],
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-  return <div>This is login.</div>;
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <button type="submit" className="btn-submit">
+          Login
+        </button>
+      </form>
+      <p>
+        New to Amazon? <Link to="/signup">Create a New Account</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
