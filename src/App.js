45889bfd8013ad4ad97d3e368c65865fb8428b10
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-17 00:47:02 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import About from './components/About/About';
-import Main from './layouts/Main';
-import Shop from './components/Shop/Shop';
-import Orders from './components/Orders/Orders';
-import Inventory from './components/Inventory/Inventory';
-import { productsAndCartLoader } from './loaders/productsAndCartLoader';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Main from "./layouts/Main";
+import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import { productsAndCartLoader } from "./loaders/productsAndCartLoader";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
+      path: "/",
       element: <Main></Main>,
       children: [
         {
-          path: '/',
-          loader: () => fetch('products.json'),
-          element: <Shop></Shop>
+          path: "/",
+          loader: () => fetch("products.json"),
+          element: <Shop />,
         },
         {
-          path:'orders',
+          path: "orders",
           loader: productsAndCartLoader,
-          element: <Orders></Orders>
+          element: <Orders />,
         },
         {
-          path: 'inventory',
-          element: <Inventory></Inventory>
+          path: "inventory",
+          element: <Inventory />,
         },
         {
-          path:'about',
-          element:<About></About>
-        }
-      ]
+          path: "about",
+          element: <About />,
+        },
+      ],
     },
-    
-  ])
+  ]);
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
